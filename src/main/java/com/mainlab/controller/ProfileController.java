6d3fc77de30a8f09f1a313505131283e81ad2098
jinspark <jinsangp@gmail.com>
2022-06-30 13:18:14 +0900

    [BE] Test prod env - Refactor

--- src/main/java/com/mainlab/controller/ProfileController.java
+++ src/main/java/com/mainlab/controller/ProfileController.java
@@ -3,10 +3,7 @@ package com.mainlab.controller;
 import com.mainlab.model.UserProfileResponse;
 import com.mainlab.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/profile")
@@ -17,7 +14,7 @@ public class ProfileController {
 
     // GET 을 쓰게되면 앞에 CF 를 두게되면 역시 캐싱하겠지.. 캐싱될 경우 Invalidation 도 어려움
     @ResponseBody
-    @RequestMapping("/{userId}")
+    @RequestMapping(value = "/{userId}", method = RequestMethod.POST)
     public UserProfileResponse getUserProfile(@PathVariable("userId") String userId) {
         return userProfileService.getCompleteUserProfile(userId);
     }
