59d0a58ea608ca26669d62932a36c2da3072758c
jinspark-lab <jinsangp@gmail.com>
2022-10-14 18:06:34 -0400

    [BE] fix AuthEnvironmentModel converting Error

--- src/main/java/com/mainlab/model/login/AuthEnvironmentModel.java
+++ src/main/java/com/mainlab/model/login/AuthEnvironmentModel.java
@@ -2,10 +2,12 @@ package com.mainlab.model.login;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthEnvironmentModel {
     private String issuer;
