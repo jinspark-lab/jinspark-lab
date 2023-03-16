a835373c81f6132dddcb18f10d94e876ec964321
jinspark-lab <jinsangp@gmail.com>
2023-03-16 11:16:28 -0400

    [Fix] Test Code Fix

--- src/test/java/com/mainlab/AppTest.java
+++ src/test/java/com/mainlab/AppTest.java
@@ -51,7 +51,7 @@ public class AppTest extends BaseAppTest {
 
     @Test
     public void testStorage() {
-        Optional<Bucket> staticBucket = storageService.getS3Client().listBuckets().buckets().stream().filter(bucket -> bucket.name().equals("jinspark-lab-static-resource-bucket")).findAny();
+        Optional<Bucket> staticBucket = storageService.getS3Client().listBuckets().buckets().stream().filter(bucket -> bucket.name().equals("jinspark-lab-resource-bucket")).findAny();
         assertTrue(staticBucket.isPresent());
     }
 
